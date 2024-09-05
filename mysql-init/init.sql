0990cf3df657ad3ccffeba4676219c319053cf71
deekondasruthi <sruthideekonda2722@gmail.com>
2024-09-05 14:50:44 +0530

    Update and rename init.txt to init.sql

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS middleware_system;
 GRANT ALL PRIVILEGES ON middleware_system.* TO 'koyembedu'@'%';
+\ GRANT ALL PRIVILEGES ON middleware_system.* TO 'koyembedu'@'%' IDENTIFIED BY 'Admin@123'; \
 
 FLUSH PRIVILEGES;
