bf0065c0f3518b4d09e61d1329d8bbae12787b8b
deekondasruthi <sruthideekonda2722@gmail.com>
2024-09-05 14:57:06 +0530

    Update init.sql

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,5 +1,3 @@
 CREATE DATABASE IF NOT EXISTS middleware_system;
 GRANT ALL PRIVILEGES ON middleware_system.* TO 'koyembedu'@'%';
-\ GRANT ALL PRIVILEGES ON middleware_system.* TO 'koyembedu'@'%' IDENTIFIED BY 'Admin@123'; \
-
 FLUSH PRIVILEGES;
